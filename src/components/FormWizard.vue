18d566a5e623ca57436e68cacfb25cb78a167d56
J. Paul Bamberg <jpbamberg1993@gmail.com>
2018-11-11 16:41:01 -0500

    Show complete order button on last step and loading animation on transition

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -1,37 +1,58 @@
 <template>
-  <div>
-		<keep-alive>
-			<component
-		 		ref="currentStep"
-				:is="currentStep"
-				@update="processStep"
-				:wizard-data="form"/>
-		</keep-alive>
+	<div>
+		<div v-if="wizardInProgress" v-show="asyncState !== 'pending'">
+			<keep-alive>
+				<component
+					ref="currentStep"
+					:is="currentStep"
+					@update="processStep"
+					:wizard-data="form"/>
+			</keep-alive>
 
-    <div class="progress-bar">
-      <div :style="`width: ${progress}%;`"></div>
-    </div>
+			<div class="progress-bar">
+				<div :style="`width: ${progress}%;`"></div>
+			</div>
 
-    <!-- Actions -->
-    <div class="buttons">
-      <button
-        @click="goBack"
-        v-if="currentStepNumber > 1"
-        class="btn-outlined"
-      >Back
-      </button>
-      <button
-        @click="goNext"
-				:disabled="!canGoNext"
-        class="btn"
-      >Next</button>
-    </div>
+			<!-- Actions -->
+			<div class="buttons">
+				<button
+					@click="goBack"
+					v-if="currentStepNumber > 1"
+					class="btn-outlined"
+				>Back
+				</button>
+				<button
+					@click="nextBtnAction"
+					:disabled="!canGoNext"
+					class="btn">
+					{{ isLastStep ? 'Complete Order' : 'Next' }}
+				</button>
+			</div>
 
-    <pre><code>{{form}}</code></pre>
-  </div>
+			<pre><code>{{form}}</code></pre>
+		</div>
+		<div v-else>
+			<h1 class="title">Thank you!</h1>
+			<h2 class="subtitle">
+				We look foward to shipping your first box
+			</h2>
+
+			<p class="text-center">
+				<a href="https://vueschool.io" target="_blank" class="btn">Go somewhere cool</a>
+			</p>
+		</div>
+		
+		<div class="loading-wrapper" v-if="asyncState === 'pending'">
+			<div class="loader">
+				<img src="/spinner.svg" alt="">
+				<p>Please wait, we're hitting our servers!</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import {postFormToDB} from '../api'
 import FormPlanPicker from './FormPlanPicker'
 import FormUserDetails from './FormUserDetails'
 import FormAddress from './FormAddress'
@@ -48,6 +69,7 @@ export default {
     return {
       currentStepNumber: 1,
 			canGoNext: false,
+			asyncState: null,
 			steps: [
 				'FormPlanPicker',
 				'FormUserDetails',
@@ -67,7 +89,13 @@ export default {
     }
   },
   computed: {
-		currentStep() {
+		isLastStep () {
+			return this.currentStepNumber === this.length
+		},
+		wizardInProgress () {
+			return this.currentStepNumber <= this.length
+		},
+		currentStep () {
 			return this.steps[this.currentStepNumber - 1]
 		},
 		length () {
@@ -78,6 +106,22 @@ export default {
     }
   },
   methods: {
+		submitOrder () {
+			this.asyncState = 'pending'
+			postFormToDB(this.form)
+				.then(() => {
+					console.log('form submitted', this.form)
+					this.asyncState = 'success'
+					this.currentStepNumber++
+				})
+		},
+		nextBtnAction () {
+			if (this.isLastStep) {
+				this.submitOrder()
+			} else {
+				this.goNext()
+			}
+		},
 		processStep (step) {
 			Object.assign(this.form, step.data)
 			this.canGoNext = step.valid
